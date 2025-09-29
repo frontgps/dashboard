e83a18e6719e57adbbab860679f0b7edf0e2392b
zahra abdi <zahraw20000@gmail.com>
2025-09-29 03:48:22 -0700

    design login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import Button from '@/components/Button/Button'
+import { Route, Routes } from "react-router-dom";
+import Login from "@/pages/Login";
 
 function App() {
   return (
     <>
+      <Routes>
+      <Route path="/" element={
+      <>
       <p>Interactive Frontend Camp</p>
       <Button>add</Button>
+      </>
+      }/>
+      <Route path="/login" element={<Login/>}/>
+      </Routes>
+
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from '@/App'
 import { ConfigProvider } from 'antd'
 import '@/styles/theme.scss'
+import { BrowserRouter } from "react-router-dom";
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ConfigProvider direction="rtl">
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
     </ConfigProvider>
   </StrictMode>
 )
