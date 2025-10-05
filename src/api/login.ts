5159346981ce24cf250e0b847a0ebd51af80a7b2
m.pakravan <mahdipakravan79@gmail.com>
2025-10-05 10:46:13 +0330

    feat / axios instance

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,12 +1,13 @@
 import axios from 'axios'
 import { BASE_URL } from './baseUrl'
 import { AuthSuccessResponse, LoginCredentials } from '@/types/auth'
+import { requestInstance } from './instance'
 
 export async function login({
   identity,
   password,
 }: LoginCredentials): Promise<AuthSuccessResponse> {
-  const response = await axios.post<AuthSuccessResponse>(
+  const response = await requestInstance.post<AuthSuccessResponse>(
     `${BASE_URL}api/collections/po_users/auth-with-password`,
     { identity, password }
   )
