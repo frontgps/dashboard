1a6ba6ff5591dcbdb5559f56393d975ee93a1089
m.pakravan <mahdipakravan79@gmail.com>
2025-10-20 15:42:39 +0330

    fix / resolved conflict

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { lazy, Suspense } from 'react';
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import ProtectedRoutes from '@/pages/ProtectedRoutes';
+import Dashboard from '@/pages/Dashboard';
 const queryClient = new QueryClient();
 
 const withSuspense = (el: React.JSX.Element) => (
@@ -12,7 +13,8 @@ const ProtectedLayout = lazy(() => import('@/layout/ProtectedLayout'));
 
 const HomePage = lazy(() => import('@/pages/Dashboard'));
 const LoginPage = lazy(() => import('@/pages/Login'));
-const UsersPage = lazy(() => import('@/pages/users/index'));
+const UsersPage = lazy(() => import('@/pages/users'));
+// const CreateUserModal = lazy(() => import('@/pages/users/create'));
 
 export const router = createBrowserRouter([
   {
@@ -25,37 +27,33 @@ export const router = createBrowserRouter([
     children: [
       {
         path: '/dashboard',
-        element: withSuspense(<HomePage />),
+        element: withSuspense(
+          <ProtectedRoutes>
+            <Dashboard />
+          </ProtectedRoutes>,
+        ),
       },
       {
         path: '/users',
-        element: withSuspense(<UsersPage />),
+        element: withSuspense(
+          <ProtectedRoutes>
+            <UsersPage />
+          </ProtectedRoutes>,
+        ),
       },
       {
-        path: '/tables',
-        element: withSuspense(<div>Tables Page</div>),
-      },
-      {
-        path: '/billing',
-        element: withSuspense(<div>Billing Page</div>),
-      },
-      {
-        path: '/rtl',
-        element: withSuspense(<div>RTL Page</div>),
-      },
-      {
-        path: '/profile',
-        element: withSuspense(<div>Profile Page</div>),
+        path: '/users/edit/:id',
+        element: withSuspense(
+          <ProtectedRoutes>
+            <UsersPage />
+          </ProtectedRoutes>,
+        ),
       },
     ],
   },
   {
     path: '/login',
     element: withSuspense(<LoginPage />),
-  },
-  {
-    path: '*',
-    element: <Navigate to="/" replace />,
   },
 
   // PLOP_INJECT_ROUTES
